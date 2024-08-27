67dd6806040145462c16443942b28d2d1b8f6f0a
Collin Arbour <collinarbour@gmail.com>
2024-08-27 14:14:40 +0200

    Adding most recent configuration files

--- ALP/mUFO/m50/crab_cfg_step3.py
+++ ALP/mUFO/m50/crab_cfg_step3.py
@@ -3,7 +3,7 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_('General')
-config.General.requestName = 'mALP_0p5_mUFO_step3'
+config.General.requestName = 'mALP_50_mUFO_step3'
 config.General.transferLogs = True
 config.General.workArea = 'crab_projects'
 
@@ -16,11 +16,12 @@ config.JobType.numCores=4
 config.section_('Data')
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 1
-config.Data.outputDatasetTag = 'mALP_0p5_mUFO_step3'
+config.Data.outputDatasetTag = 'mALP_50_mUFO_step3'
 config.Data.inputDBS = 'phys03'
-config.Data.outLFNDirBase = '/store/user/carbour/mc/alp/mUFO/m0p5/'
+config.Data.outLFNDirBase = '/store/user/carbour/mc/alp/mUFO/m50/'
 config.Data.publication = True
-config.Data.inputDataset = '/CRAB_PrivateMC/carbour-mALP_0p5_mUFO_step2-87e245638d0e8549d13398f07ce44cec/USER'
+
+config.Data.inputDataset = '/CRAB_PrivateMC/carbour-mALP_50_mUFO_step2-87e245638d0e8549d13398f07ce44cec/USER'
 
 config.section_('Site')
 config.Site.storageSite = 'T2_CH_CERN'
--- NMSSM/m0p5/crab_cfg_step1.py
+++ NMSSM/m0p5/crab_cfg_step1.py
@@ -9,9 +9,9 @@ config.General.workArea = 'crab_projects'
 
 config.section_('JobType')
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'Hadronizer_TuneCUETP8M1_13TeV_generic_LHE_pythia8_cff_py_GEN_SIM.py'
+config.JobType.psetName = 'step1.py'
 config.JobType.maxMemoryMB = 10000
-config.JobType.numCores=4
+config.JobType.numCores=8
 
 config.section_('Data')
 config.Data.splitting = 'FileBased'
@@ -21,7 +21,7 @@ config.Data.inputDBS = 'phys03'
 config.Data.outLFNDirBase = '/store/user/carbour/mc/NMSSM/m0p5/'
 config.Data.publication = True
 
-config.Data.inputDataset = '/CRAB_PrivateMC/carbour-mALP_0p5_mUFO_step0-3f22eb42fbc8c953391827da6f10333b/USER'
+config.Data.inputDataset = '/CRAB_PrivateMC/carbour-NMSSM_m0p5_step0-ee18b858813a9051a89a1b5997e11fd7/USER'
 
 config.section_('Site')
 config.Site.storageSite = 'T2_CH_CERN'
